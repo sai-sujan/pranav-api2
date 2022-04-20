eca0446b9c31eb90bb94bbdd91d1d1c2b77cedcf
sai-sujan <sujan557712@gmail.com>
2022-04-20 23:30:55 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def main():
+    if request.method == 'GET':
+        df = pd.read_csv("SpotifyAudioFeaturesApril2019.csv")
+        df = df.sort_values(by=['popularity'], ascending=True)
+        df = df['track_name']
+        print(df)
+
+    return df.to_json()
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
